5b3283bec1dd04c37d56b23b984dfa632e4f0534
unknown <busan@CC811215031.BNKFGEX.com>
2025-06-12 15:51:34 +0900

    add ch07

--- React/ch07/src/components/UserRegisterComponent.jsx
+++ React/ch07/src/components/UserRegisterComponent.jsx
@@ -1,34 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function UserRegisterComponent() {
+  const navigate = useNavigate();
+
+  const [user, setUser] = useState({
+    uid: "",
+    name: "",
+    birth: "",
+    age: 0,
+  });
+
+  const changeHandler = (e) => {
+    e.preventDefault();
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    // json-server 등록
+    fetch(`http://localhost:3000/users`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <div className="UserRegisterComponent">
       <h4>회원등록</h4>
-      <form>
+      <form onSubmit={submitHandler}>
         <table border={1}>
           <tbody>
             <tr>
               <td>아이디</td>
               <td>
-                <input type="text" name="uid" value={null} onChange={null} />
+                <input
+                  type="text"
+                  name="uid"
+                  value={user.uid}
+                  onChange={changeHandler}
+                />
               </td>
             </tr>
             <tr>
               <td>이름</td>
               <td>
-                <input type="text" name="name" value={null} onChange={null} />
+                <input
+                  type="text"
+                  name="name"
+                  value={user.name}
+                  onChange={changeHandler}
+                />
               </td>
             </tr>
             <tr>
               <td>생년월일</td>
               <td>
-                <input type="date" name="birth" value={null} onChange={null} />
+                <input
+                  type="date"
+                  name="birth"
+                  value={user.birth}
+                  onChange={changeHandler}
+                />
               </td>
             </tr>
             <tr>
               <td>나이</td>
               <td>
-                <input type="number" name="age" value={null} onChange={null} />
+                <input
+                  type="number"
+                  name="age"
+                  value={user.age}
+                  onChange={changeHandler}
+                />
               </td>
             </tr>
             <tr>
